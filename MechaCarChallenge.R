5b2422f82b4213aaf2592481f4477c2c32981922
EHuiMartinez <69170347+EHuiMartinez@users.noreply.github.com>
2021-01-21 16:43:39 -0500

    update

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,3 +2,10 @@ library(dplyr)
 MechaCar <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors=F) #read in csv
 lm(vehicle_length ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg ,data=MechaCar) #generate multiple linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar)) #generate summary
+
+SuspensionCoil <- read.csv(file='Suspension_Coil.csv') #read in csv
+library(tidyverse)
+total_summary <- SuspensionCoil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI)) #create total summary table
+lot_summary <- SuspensionCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+
--- README.md
+++ README.md
@@ -9,3 +9,13 @@
 --Does this linear model predict mpg of MechaCar prototypes effectively? Why or why not?
 
 ![Deliverable1_LM.png](Deliverable1_LM.png)
+
+## Summary Statistics on Suspension Coils
+
+--write a short summary using screenshots from your total_summary and lot_summary dataframes, and address the following question:
+
+--The design specifications for the MechaCar suspension coils dictate that the variance of the suspension coils must not exceed 100 pounds per square inch. Does the current manufacturing data meet this design specification for all manufacturing lots in total and each lot individually? Why or why not?
+
+![Deliverable2_total_summary.png](Deliverable2_total_summary.png)
+
+![Deliverable2_lot_summary.png](Deliverable2_lot_summary.png)
